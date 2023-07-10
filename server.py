fe614f862268c1f0aaf2197b66e933a9da9c03b0
Server 7 <fil7@node2.cluster.local>
2023-07-10 20:08:25 +0700

    update api

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import re
 from vncorenlp import VnCoreNLP
-from flask import Flask , request, redirect, url_for
+import time
+import json
+from flask import Flask , request, jsonify
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from constants import *
 
@@ -45,6 +47,12 @@ def preprocess(text, tokenized=True, lowercased=True):
         text = pre_text
     return text
 
+def write_to_file(product_id: int, review: str, user_id: int, task2: int):
+    log_entry = f"{time.monotonic()},^,{product_id},^,{user_id},^,{review},^,{task2}"
+    # Write log entry to a text file
+    with open('log_file.txt', 'a') as file:
+        file.write(log_entry + '\n')
+        
 @app.route('/api/task1', methods=['POST'])
 def handle_comment_task_1():
     if request.method == "POST":
@@ -58,11 +66,48 @@ def handle_comment_task_1():
 @app.route('/api/task2', methods=['POST'])
 def handle_comment_task_2():
     if request.method == "POST":
-        review = request.json['review']
+        data = review = request.json['review']
+        productID = request.json['product_id']
+        userID = request.json['user_id']
         review = preprocess(review, tokenized=True, lowercased=False)
         tokenized_input = tokenizer(str(review), truncation=True, padding=True, max_length=100, return_tensors="pt")
         outputs = model_task_2(**tokenized_input)
         predicted_class_id = outputs.logits.argmax().item()
+        if predicted_class_id in [0,1,2,3]:
+            write_to_file(product_id=productID, user_id=userID, review=data, task2=predicted_class_id)
         return str(predicted_class_id), 200
+    
+def convert_to_array(line):
+    line_data = line.strip().split(',^,')
+    return line_data
+
+def custom_sort(elem):
+    priority = int(elem[-1])
+    time = float(elem[0])
+    return (priority, -time)
+
+@app.route('/api/get/<user_id>/<product_id>', methods=['GET'])
+def get_all_data(user_id, product_id):
+    data = []
+    with open('log_file.txt', 'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            data.append(convert_to_array(line))
+        sorted_data = sorted(data, key=custom_sort)
+        filtered_data = []
+        for item in sorted_data:
+            if item[1] == product_id and item[2] == user_id:
+                filtered_data.append(item)
+        json_data = []
+        for item in filtered_data:
+            if item != None:
+                item_dict = {
+                'product_id': item[1],
+                'user_id': item[2],
+                'review': item[3],
+                'task2': item[4]
+                }
+                json_data.append(json.loads(json.dumps(item_dict)))
+        return jsonify(json_data)
     
 app.run(host='0.0.0.0',debug=False, port=int(os.environ.get('PORT', 8081)))
